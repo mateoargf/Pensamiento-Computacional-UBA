4ed93207df38de99ee57e0338bc2d5b5cdc88e60
Mateo Gómez Fleytas <101438175+mateoargf@users.noreply.github.com>
2025-01-25 11:56:23 -0300

    ejercicio 1 completado

---
+++ guía 2 tipos de datos, expresiones y funciones/Ejercicios/1.py
@@ -0,0 +1,7 @@
+# 1. Crear un programa que le solicite al usuario un entero y lo imprima por pantalla.
+# Recordá que podés usar las funciones input (para solicitar información) y print para mostrar
+# información
+
+entero = int(input('ingrese un número entero: '))
+
+print(f'su dato ingresado es de tipo: {type(entero)}')
---
+++ guía 2 tipos de datos, expresiones y funciones/Ejercicios/2.py
@@ -0,0 +1,8 @@
+# 2. Crear un programa que le solicite al usuario dos números enteros y luego imprima por pantalla:
+# ● la suma de ambos números
+# ● la resta de ambos números
+# ● la multiplicación de ambos números
+# ● la división entera de ambos números
+# ● el resto
+# Más adelante podríamos crear nuestra propia calculadora :)
+
