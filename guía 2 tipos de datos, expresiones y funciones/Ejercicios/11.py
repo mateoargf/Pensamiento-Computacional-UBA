a6e6126fd34846b18b2826da9ff1fc6ec71a3d74
Mateo Gómez Fleytas <101438175+mateoargf@users.noreply.github.com>
2025-01-26 15:09:18 -0300

    ejer. 11 finalizado

--- guía 2 tipos de datos, expresiones y funciones/Ejercicios/11.py
+++ guía 2 tipos de datos, expresiones y funciones/Ejercicios/11.py
@@ -1,1 +1,12 @@
 # 11. Obtener una palabra e imprimir los primeros 5 caracteres (pista: slicear la palabra).
+
+# Recibe un parámetro palabra y devuelve los primeros 5 caracteres
+def devolver_caracteres(palabra):
+     
+     return palabra[:5:]
+
+palabra = input('Ingrese una palabra: ')
+
+cinco_caracteres = devolver_caracteres(palabra)
+
+print(cinco_caracteres)
