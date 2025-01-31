5b7844f755d8e5c5ef840794dfd1092423b34f04
Mateo Gómez Fleytas <101438175+mateoargf@users.noreply.github.com>
2025-01-31 15:28:31 -0300

    clase de operadores y expresiones

---
+++ guía 3 ciclos y rangos y condicionales/operadores_y_expresiones.py
@@ -0,0 +1,25 @@
+# Operadores
+# - aritméticos -> + - * / // %
+# - relacionales -> == != > < >= <= 
+# - lógicos -> and (y), or (o), not (no)
+
+# Expresión
+# numero_1 = 12
+# numero_2 = 45
+# numero_3 = 4
+# numero_4 = 76
+# numero_5 = 34
+
+# expresión 1)
+# numero_1 > numero_3 -> verdad -> True
+
+# expresión 2)
+# numero_1 == numero_2 -> falso -> False
+
+# expresión 3)
+# (numero_2 < numero_4) and (numero_3 == numero_5) 
+#           True        and            False        -> falso -> False      
+
+# expresión 4)
+# (numero_1 + numero_5 == numero_2) or (numero_3 >= numero_4)
+#                       False            or          False    -> falso -> False           
