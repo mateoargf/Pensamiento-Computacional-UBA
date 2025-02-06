aa3ce87e8a848864ee8a4a5cf0e5f4650d5ce7c1
Mateo Gómez Fleytas <101438175+mateoargf@users.noreply.github.com>
2025-02-06 16:25:22 -0300

    uni 4, parte 1: ej 3

---
+++ guía 4 secuencias, tuplas y listas/ejercicios/parte 1/3.py
@@ -0,0 +1,7 @@
+# 3. Crear una secuencia con números distintos, y luego devolver el elemento máximo y el mínimo.
+
+tupla = (1,244,35,455.55,12,456)
+
+mayor= max(tupla)
+
+print(f'EL mayor valor de la secuencia tupla es: {mayor}')
