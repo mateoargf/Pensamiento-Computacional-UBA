b214ec6647ef3ad0ab2bfca7bf73f21028ffd1ea
Mateo Gómez Fleytas <101438175+mateoargf@users.noreply.github.com>
2025-02-06 16:30:41 -0300

    uni 4, parte 1: ej 5

---
+++ guía 4 secuencias, tuplas y listas/ejercicios/parte 1/5.py
@@ -0,0 +1,5 @@
+# 5. Crear una tupla que guarde tu nombre y tu edad. Luego, imprimir por pantalla tu edad, accediendo al elemento de la tupla que corresponda.
+
+tupla = ('Mateo', 24)
+
+print(tupla[1])
