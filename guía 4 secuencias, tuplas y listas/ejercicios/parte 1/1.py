c91b3d849755f4fe2e3a351aa89f7013e139b8b3
Mateo Gómez Fleytas <101438175+mateoargf@users.noreply.github.com>
2025-02-06 16:21:46 -0300

    uni 4, parte 1: ej 2

--- guía 4 secuencias, tuplas y listas/ejercicios/parte 1/1.py
+++ guía 4 secuencias, tuplas y listas/ejercicios/parte 1/1.py
@@ -1,3 +1,6 @@
+# 1. Crear una lista con los números del 1 al 10. Acceder con el índice a la posición que contiene el número
+# 5, e imprimirlo por pantalla. Recordar que el índice de las listas empiezan con 0
+
 numeros = [1,2,3,4,5,6,7,8,9,10]
 
 posicion = numeros.index(5)
---
+++ guía 4 secuencias, tuplas y listas/ejercicios/parte 1/2.py
@@ -0,0 +1,7 @@
+# 2. Con la lista del punto anterior, usar la función len() para averiguar su longitud, e imprimirla.
+
+numeros = [1,2,3,4,5,6,7,8,9,10]
+
+longitud = len(numeros)
+
+print(f'La longitud de números es: {longitud}')
