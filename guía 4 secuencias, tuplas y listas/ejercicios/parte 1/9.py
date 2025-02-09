bc7b9d755509efe183e58938d9cd16cc6ce5424a
Mateo Gómez Fleytas <101438175+mateoargf@users.noreply.github.com>
2025-02-09 18:44:35 -0300

    uni 4, parte 1: ej. 9

--- guía 4 secuencias, tuplas y listas/ejercicios/parte 1/9.py
+++ guía 4 secuencias, tuplas y listas/ejercicios/parte 1/9.py
@@ -3,22 +3,24 @@
 
 # Recibe un parámetro y verifica e informa si se repiten libros y los imprima con nombre y cuantos son
 def ver_stock(libreria):
+     lista = []
      for i in libreria:
-          cont = 0
-          for j in libreria:
-               if i == j:
-                    cont +=1
-          return cont
-
-def validar_repetido(libreria):
-     for i in libreria:
-          if i != ver_stock(libreria):
-               return True
-          else:
-               return False
+          contador = libreria.count(i)
+          if contador == 1:
+               lista.append(i)
+               print(f'Nombre: {i} - Cantidad: {contador}')
+          elif contador > 1:
+               valido = True
+               for j in lista:
+                    if j == i:
+                         valido = False
+               if valido == True: 
+                    lista. append(i)
+                    print(f'Nombre: {i} - Cantidad: {contador}')
+                         
+                      
           
-          
-          
+  
 libros = ["Cien años de soledad", "Drácula", "Don Quijote de la Mancha", "Moby-Dick", "1984", "Orgullo y prejuicio", "Crimen y castigo", "El retrato de Dorian Gray", "Los miserables", "Fahrenheit 451", "Don Quijote de la Mancha", "Moby-Dick", "1984", "Moby-Dick", "1984", ]
 
-info = validar_repetido(libros)
+ver_stock(libros)
