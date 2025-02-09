7180150dc533d51d5c2e414c2fbf1419713c6018
Mateo Gómez Fleytas <101438175+mateoargf@users.noreply.github.com>
2025-02-09 00:39:08 -0300

    guia 4, parte 1: ej. 7

---
+++ guía 4 secuencias, tuplas y listas/ejercicios/parte 1/7.py
@@ -0,0 +1,13 @@
+# 7. Se pide ahora crear 3 tuplas como las del ejercicio 5, con un nombre y una edad. A continuación,
+# guardarlas en una lista. Pensar, ¿De que nos servirá guardar las tuplas en una lista en vez de tenerlas
+# por separado?
+
+
+tupla_1 = ('Mateo', 24)
+tupla_2 = ('Jazmín', 24)
+tupla_3 = ('Nazareno', 28)
+
+personas= [tupla_1,tupla_2,tupla_3]
+
+# Podemos iterar la lista con el nombre y edad de cada persona y al estar almacenadas en una variable que las represente, tenemos acceso de manera mas sencilla a cada uno de los elementos si queremos operar con todos/ algunos de ellos... 
+# En un futuro si amplío la cantidad de personas en la lista acceder a ellas se hace mas notorio si se encuentran almacenadas en un lugar para poder operar...
