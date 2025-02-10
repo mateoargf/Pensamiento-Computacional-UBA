f133ac629d4dcd8c4b77c2d5376eb29439c68478
Mateo Gómez Fleytas <101438175+mateoargf@users.noreply.github.com>
2025-02-10 17:00:53 -0300

    hacer guia 4, parte 1: ej. 13 pendiente...

---
+++ guía 4 secuencias, tuplas y listas/ejercicios/parte 1/13.py
@@ -0,0 +1,7 @@
+# 13. Se tiene un ticket de supermercado que se puede representar como una lista de tuplas (producto,
+# precio).
+# a. Hacer una función que reciba la lista, calcule y devuelva el total que hay que pagar.
+# b. Ahora se tienen dos tickets. Juntar ambos y volver a calcular el total.
+
+# Un ejemplo de lista puede ser: [(“Detergente”, 123), (“Jabón Líquido”, 456)] y nos tendría que devolver 579. (No copien y peguen la lista de la guía, porque hay caracteres que no los va a reconocer el editor de texto).
+
