a62c605de4ce69f971fd7aea119e6ce4c929fac2
Mateo Gómez Fleytas <101438175+mateoargf@users.noreply.github.com>
2025-02-12 16:47:54 -0300

    guia 4, parte 1 finalizado

--- guía 4 secuencias, tuplas y listas/ejercicios/parte 1/13.py
+++ guía 4 secuencias, tuplas y listas/ejercicios/parte 1/13.py
@@ -5,3 +5,19 @@
 
 # Un ejemplo de lista puede ser: [(“Detergente”, 123), (“Jabón Líquido”, 456)] y nos tendría que devolver 579. (No copien y peguen la lista de la guía, porque hay caracteres que no los va a reconocer el editor de texto).
 
+# Recibe de parámetro la tupla productos, calcula y devuelve el total a pagar
+def calcular_pago(lista1, lista2):
+     total = 0
+     lista = []
+     lista.extend(lista1)
+     lista.extend(lista2)
+     for i in lista:
+          total += i[1]
+
+     return print(f'Su total a pagar es: {total}$')
+
+productos_1 = [("Arroz", 250), ("Leche", 320), ("Pan", 180), ("Aceite", 850), ("Azúcar", 400), ("Fideos", 275), ("Sal", 150), ("Harina", 220), ("Cereal", 600), ("Yogur", 450)]
+
+productos_2 = [("Shampoo", 750), ("Papel Higiénico", 900), ("Queso", 1200), ("Café", 1100), ("Galletas", 500), ("Jabón en Barra", 300), ("Mayonesa", 650), ("Tomate", 350), ("Cerveza", 1300), ("Pescado", 2500)]
+
+calcular_pago(productos_1,productos_2)
