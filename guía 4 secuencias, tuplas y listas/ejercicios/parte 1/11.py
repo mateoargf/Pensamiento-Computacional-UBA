158b51046a5e0202337a060e4d295a2f210fd092
Mateo Gómez Fleytas <101438175+mateoargf@users.noreply.github.com>
2025-02-14 18:17:18 -0300

    otra forma de castear la lista a string e invertir la devolucion con los métodos reverse() de lista y ''.join() de string

--- guía 4 secuencias, tuplas y listas/ejercicios/parte 1/11.py
+++ guía 4 secuencias, tuplas y listas/ejercicios/parte 1/11.py
@@ -4,15 +4,20 @@
 # las palabras desde el final de la lista hasta el principio con un “ ” (espacio) entre cada una, para formar la frase. (ver funciones de listas y strings).
 
 # Recibe un parámetro y devuelve la lista con espacio entre elementos
-def unir_oracion(lista):
+def formar_frase_a(lista):
      lista.reverse()
      oracion = ""
      for n in lista:
           oracion += n + " "
      return oracion
 
+def formar_frase_b(lista):
+     lista.reverse()
+     frase = ' '.join(lista)
+     return frase
+
 palabras = ['entender', 'pueden', 'humanos', 'los', 'que', 'código', 'escriben', 'programadores', 'buenos', 'Los', 'entiende.', 'computadora', 'una', 'que', 'código', 'escribe', 'tonto', 'Cualquier']
 
-frase = unir_oracion(palabras)
+reflexion = formar_frase_b(palabras)
 
-print(frase)
+print(reflexion)
