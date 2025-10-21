aeda1715f8d12d100bc844acca96f9e24bea6e44
Mateo Gomez Fleytas <mateogf@hotmail.com>
2025-10-21 13:15:47 -0300

    en curso: guía 7, ejrc. 5

--- guía 7 diccionarios/ejercicios/4.py
+++ guía 7 diccionarios/ejercicios/4.py
@@ -4,3 +4,35 @@
 # vio.
 # Hacer una función que reciba el diccionario de las películas que vió Sol, y que devuelva una nueva lista
 # de diccionarios donde sólo estén las películas que tienen puntaje mayor a 7.
+
+def pelis_fav(listado):
+   mejores_pelis = []
+   for peli in listado:
+      if peli['puntaje'] > 7:
+         mejores_pelis.append(peli)
+   print(mejores_pelis)
+
+def guardad_peli(listado, nombre, fecha_estreno, puntaje):
+   peli_nueva = {
+      'nombre': nombre,
+      'estreno': fecha_estreno,
+      'puntaje': puntaje
+   }
+   listado.append(peli_nueva)
+
+peliculas = []
+
+guardad_peli(peliculas, 'Piratas del caribe', 2003, 10)
+guardad_peli(peliculas, "El Señor de los Anillos", 2001, 10)
+guardad_peli(peliculas, "Matrix", 1999, 9)
+guardad_peli(peliculas, "Inception", 2010, 7)
+guardad_peli(peliculas, "Interstellar", 2014, 9)
+guardad_peli(peliculas, "Avengers: Endgame", 2019, 6)
+guardad_peli(peliculas, "Gladiador", 2000, 9)
+guardad_peli(peliculas, "Forrest Gump", 1994, 7.5)
+guardad_peli(peliculas, "Jurassic Park", 1993, 8)
+guardad_peli(peliculas, "Toy Story", 1995, 8)
+guardad_peli(peliculas, "Catwoman", 2004, 5)
+guardad_peli(peliculas, "The Room", 2003, 3)
+
+pelis_fav(peliculas)
---
+++ guía 7 diccionarios/ejercicios/5.py
@@ -0,0 +1,12 @@
+# 5. Un profesor guarda las notas del primer parcial de sus alumnos en una lista de diccionarios que guarda
+# la siguiente información:
+# ● Nombre
+# ● Apellido
+# ● Intento
+# ● Nota
+# Donde ”intento” es la instancia que está rindiendo, 1 si es la primera vez que rinde el parcial, 2 si es el
+# primer recuperatorio y 3 si es el segundo recuperatorio.
+# Se pide hacer una función que, dado esta lista de diccionarios, devuelva el promedio de las notas en la
+# primera oportunidad que rindieron los alumnos.
+# ¿Cómo harían para generalizar la función y que el intento sea parametrizable? Es decir, que no
+# solamente sirve para el intento 1, sino que también pueda servir para los demás.
