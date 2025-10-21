db57d44c8f17e150e0b3b93b58dc9d8bd96024b3
Mateo Gomez Fleytas <mateogf@hotmail.com>
2025-10-21 13:44:35 -0300

    en curso: guía 7, ejrc. 6

--- guía 7 diccionarios/ejercicios/5.py
+++ guía 7 diccionarios/ejercicios/5.py
@@ -10,3 +10,92 @@
 # primera oportunidad que rindieron los alumnos.
 # ¿Cómo harían para generalizar la función y que el intento sea parametrizable? Es decir, que no
 # solamente sirve para el intento 1, sino que también pueda servir para los demás.
+
+def promedio(listado):
+   cant_notas = 0
+   total = 0
+   for alumno in listado:
+      if alumno['instancia'] == 1:
+         cant_notas = cant_notas + 1
+         total = total + alumno['nota']
+   return total / cant_notas
+   
+         
+         
+
+notas = [
+    {
+       "nombre": "Mateo",
+       "apellido": "Gomez",
+       "instancia": 1,
+       "nota": 10
+       },
+    {
+       "nombre": "Mateo",
+       "apellido": "Sujatovich",
+       "instancia": 2,
+       "nota": 8
+       },
+    {
+       "nombre": "Mateo",
+       "apellido": "Gonzalez",
+       "instancia": 3,
+       "nota": 9
+       },
+    {
+       "nombre": "Ana",
+       "apellido": "Lopez",
+       "instancia": 1,
+       "nota": 6
+       },
+    {
+       "nombre": "Ana",
+       "apellido": "Saenz",
+       "instancia": 2,
+       "nota": 7
+       },
+    {
+       "nombre": "Ana",
+       "apellido": "Beltran",
+       "instancia": 3,
+       "nota": 8
+       },
+    {
+       "nombre": "Juan",
+       "apellido": "Perez",
+       "instancia": 1,
+       "nota": 5
+       },
+    {
+       "nombre": "Juan",
+       "apellido": "Sasaki",
+       "instancia": 2,
+       "nota": 6
+       },
+    {
+       "nombre": "Juan",
+       "apellido": "Pedel",
+       "instancia": 3,
+       "nota": 7
+       },
+    {
+       "nombre": "Sofia",
+       "apellido": "Martinez",
+       "instancia": 1,
+       "nota": 9
+       },
+    {
+       "nombre": "Sofia",
+       "apellido": "De Elia",
+       "instancia": 2,
+       "nota": 8
+       },
+    {
+       "nombre": "Sofia",
+       "apellido": "Matarazzo",
+       "instancia": 3,
+       "nota": 10
+       },
+]
+
+print(promedio(notas))
---
+++ guía 7 diccionarios/ejercicios/6.py
@@ -0,0 +1,11 @@
+# 6. En una fábrica, se hace un chequeo de calidad a los productos antes de cada entrega. El resultado del
+# chequeo de la entrega se guarda en una lista de diccionarios, donde cada diccionario tiene la siguiente
+# información de cada producto:
+# ● Código del producto
+# ● Fecha de vencimiento
+# ● Si pasó el chequeo de calidad o no
+# Se pide hacer una función que reciba esta lista de diccionarios y elimine todos los productos que no
+# pasaron el chequeo de calidad. Devolver en una tupla el diccionario con los elementos eliminados y la
+# cantidad de elementos que quedaron en el diccionario.
+# Dado que la tupla es inmutable y nosotros no podemos ir agregando elementos a una tupla, ¿En qué
+# momento deberíamos crear la tupla?
